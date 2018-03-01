9c1c0d3310b83112a0118ab7697fb5437f2aa0aa
diana <dpa6@njit.edu>
2018-02-28 23:58:27 -0500

    Procfile for Heroku

---
+++ app/Http/Controllers/PagesController.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PagesController extends Controller
+{
+    function index()
+    {
+        return view('pages.index');
+    }
+
+    function about()
+    {
+        return view('pages.about');
+    }
+
+    function contact()
+    {
+        return view('pages.contact');
+    }
+
+    function store(Request $request)
+    {
+        $name = $request->name;
+
+        return redirect()->route('thanks', ['name' => $name]);
+    }
+
+    function thanks($name, Request $request)
+    {
+        return view('pages.thankyou')->with(compact('name'));
+    }
+
+    function logout()
+    {
+        return view('pages.logout');
+    }
+
+    function SignIn()
+    {
+        return view('pages.SignIn');
+    }
+
+    function Signup()
+    {
+        return view('pages.Signup');
+    }
+
+    function viewmessages()
+    {
+        return view('pages.viewmessages');
+    }
+
+
+}
